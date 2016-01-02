8be5243a713355b4ece57ffe74ee7b1af04957af
Geoffrey Liu <geoffliu@uw.edu>
2016-01-01 16:25:55 -0800

    begin work on updating the board

--- canvas.coffee
+++ canvas.coffee
@@ -4,6 +4,7 @@ BOARD_WIDTH = 40
 BOARD_HEIGHT = 30
 
 CALC_BUFFER = 2
+STEP_BUFFER_SIZE = 2
 
 # functions
 drawGrid = () ->
@@ -20,12 +21,24 @@ getMousePos = (canvas, evt) ->
     y: evt.clientY - rect.top
   }
 
+updateCells = () ->
+  for boardRow, i in board
+    for boardCell, j in boardRow
+      if boardCell
+        fillCell i, j
+      else
+        clearCell i, j
+
+  # to quit JSLint from complaining
+  undefined
+
 toggleCell = (rawX, rawY) ->
   cellX = Math.floor(rawX / SQUARE_SIZE) + CALC_BUFFER
   cellY = Math.floor(rawY / SQUARE_SIZE) + CALC_BUFFER
 
   board[cellX][cellY] = !board[cellX][cellY]
 
+  # TODO: should be called from event handler
   if board[cellX][cellY]
     fillCell rawX, rawY
   else
@@ -42,10 +55,66 @@ clearCell = (rawX, rawY) ->
   ctx.clearRect x, y, SQUARE_SIZE - 1, SQUARE_SIZE - 1
 
 update = () ->
-  # TODO
+  console.log board
+  for i in [0..board.length-1]
+    bufferRow = STEP_BUFFER_SIZE - 1
+    if i < STEP_BUFFER_SIZE
+      bufferRow = i
+    else
+      # need to write the contents of the first row of the buffer
+      # into the (i - STEP_BUFFER_SIZE)th row of the board
+      boardWrite = stepBuffer.shift()
+      for col in [0..boardWrite.length-1]
+        board[i-STEP_BUFFER_SIZE][col] = boardWrite[col]
+
+      # add a new row to the buffer
+      stepBuffer.push(new Array(BOARD_WIDTH+2*CALC_BUFFER))
+
+    for j in [0..board[i].length-1]
+      thisCellIsLive = isLive(i, j)
+      liveList = getLiveNeighbors(i, j)
+
+      stepBuffer[bufferRow][j] = applyRules(thisCellIsLive, liveList)
+
+  # buffer still stores the last STEP_BUFFER_SIZE calculated rows,
+  # so we need to write those into the board
+  for buffI in [0..stepBuffer.length-1]
+    for buffJ in [0..stepBuffer[buffI].length-1]
+      board[board.length-STEP_BUFFER_SIZE+buffI][buffJ] = stepBuffer[buffI][buffJ]
+
+  console.log board
+
+  # TODO: should be called from event handler
+  updateCells()
 
 getLiveNeighbors = (row, col) ->
-  # TODO
+  live = []
+  for i in [row-1..row+1]
+    for j in [col-1..col+1]
+      if isLive(i, j)
+        live.push([i, j])
+
+  return live
+
+isLive = (row, col) ->
+  if isInBounds(row, col)
+    return board[row][col] == true
+
+  return false
+
+isInBounds = (row, col) ->
+  return row >= 0 && col >= 0 && row < board.length && col < board[row].length
+
+applyRules = (cellLive, liveList) ->
+  numAlive = liveList.length
+  if numAlive < 2 && cellLive
+    return false
+  if 2 <= numAlive <= 3 && cellLive
+    return true
+  if numAlive > 3 && cellLive
+    return false
+  if !cellLive && numAlive == 3
+    return true
 
 # MAIN CODE
 # set board size
@@ -63,7 +132,8 @@ drawGrid()
 board = new Array(BOARD_HEIGHT+2*CALC_BUFFER)
 board[i] = new Array(BOARD_WIDTH+2*CALC_BUFFER) for i in [0..board.length-1]
 
-# console.log board
+stepBuffer = new Array(STEP_BUFFER_SIZE)
+stepBuffer[i] = new Array(BOARD_WIDTH+2*CALC_BUFFER) for i in [0..stepBuffer.length-1]
 
 # Listen for user input
 
@@ -76,4 +146,7 @@ document.getElementById('step').addEventListener('click', update, false)
 document.getElementById('reset').addEventListener('click', () ->
   ctx.clearRect 0, 0, canvas.width, canvas.height
   drawGrid()
+
+  board = new Array(BOARD_HEIGHT+2*CALC_BUFFER)
+  board[i] = new Array(BOARD_WIDTH+2*CALC_BUFFER) for i in [0..board.length-1]
 , false)
--- canvas.js
+++ canvas.js
@@ -1,5 +1,5 @@
 (function() {
-  var BOARD_HEIGHT, BOARD_WIDTH, CALC_BUFFER, SQUARE_SIZE, board, canvas, clearCell, ctx, drawGrid, drawLine, fillCell, getLiveNeighbors, getMousePos, i, j, ref, toggleCell, update;
+  var BOARD_HEIGHT, BOARD_WIDTH, CALC_BUFFER, SQUARE_SIZE, STEP_BUFFER_SIZE, applyRules, board, canvas, clearCell, ctx, drawGrid, drawLine, fillCell, getLiveNeighbors, getMousePos, i, isInBounds, isLive, k, l, ref, ref1, stepBuffer, toggleCell, update, updateCells;
 
   SQUARE_SIZE = 30;
 
@@ -9,13 +9,15 @@
 
   CALC_BUFFER = 2;
 
+  STEP_BUFFER_SIZE = 2;
+
   drawGrid = function() {
-    var i, j, k, ref, ref1, ref2, ref3, results;
-    for (i = j = 0.5, ref = canvas.width + 0.5, ref1 = SQUARE_SIZE; ref1 > 0 ? j <= ref : j >= ref; i = j += ref1) {
+    var i, k, l, ref, ref1, ref2, ref3, results;
+    for (i = k = 0.5, ref = canvas.width + 0.5, ref1 = SQUARE_SIZE; ref1 > 0 ? k <= ref : k >= ref; i = k += ref1) {
       drawLine(ctx, i, 0, i, canvas.height);
     }
     results = [];
-    for (i = k = 0.5, ref2 = canvas.height + 0.5, ref3 = SQUARE_SIZE; ref3 > 0 ? k <= ref2 : k >= ref2; i = k += ref3) {
+    for (i = l = 0.5, ref2 = canvas.height + 0.5, ref3 = SQUARE_SIZE; ref3 > 0 ? l <= ref2 : l >= ref2; i = l += ref3) {
       results.push(drawLine(ctx, 0, i, canvas.width, i));
     }
     return results;
@@ -34,6 +36,22 @@
       x: evt.clientX - rect.left,
       y: evt.clientY - rect.top
     };
+  };
+
+  updateCells = function() {
+    var boardCell, boardRow, i, j, k, l, len, len1;
+    for (i = k = 0, len = board.length; k < len; i = ++k) {
+      boardRow = board[i];
+      for (j = l = 0, len1 = boardRow.length; l < len1; j = ++l) {
+        boardCell = boardRow[j];
+        if (boardCell) {
+          fillCell(i, j);
+        } else {
+          clearCell(i, j);
+        }
+      }
+    }
+    return void 0;
   };
 
   toggleCell = function(rawX, rawY) {
@@ -62,9 +80,75 @@
     return ctx.clearRect(x, y, SQUARE_SIZE - 1, SQUARE_SIZE - 1);
   };
 
-  update = function() {};
+  update = function() {
+    var boardWrite, buffI, buffJ, bufferRow, col, i, j, k, l, liveList, m, n, o, ref, ref1, ref2, ref3, ref4, thisCellIsLive;
+    console.log(board);
+    for (i = k = 0, ref = board.length - 1; 0 <= ref ? k <= ref : k >= ref; i = 0 <= ref ? ++k : --k) {
+      bufferRow = STEP_BUFFER_SIZE - 1;
+      if (i < STEP_BUFFER_SIZE) {
+        bufferRow = i;
+      } else {
+        boardWrite = stepBuffer.shift();
+        for (col = l = 0, ref1 = boardWrite.length - 1; 0 <= ref1 ? l <= ref1 : l >= ref1; col = 0 <= ref1 ? ++l : --l) {
+          board[i - STEP_BUFFER_SIZE][col] = boardWrite[col];
+        }
+        stepBuffer.push(new Array(BOARD_WIDTH + 2 * CALC_BUFFER));
+      }
+      for (j = m = 0, ref2 = board[i].length - 1; 0 <= ref2 ? m <= ref2 : m >= ref2; j = 0 <= ref2 ? ++m : --m) {
+        thisCellIsLive = isLive(i, j);
+        liveList = getLiveNeighbors(i, j);
+        stepBuffer[bufferRow][j] = applyRules(thisCellIsLive, liveList);
+      }
+    }
+    for (buffI = n = 0, ref3 = stepBuffer.length - 1; 0 <= ref3 ? n <= ref3 : n >= ref3; buffI = 0 <= ref3 ? ++n : --n) {
+      for (buffJ = o = 0, ref4 = stepBuffer[buffI].length - 1; 0 <= ref4 ? o <= ref4 : o >= ref4; buffJ = 0 <= ref4 ? ++o : --o) {
+        board[board.length - STEP_BUFFER_SIZE + buffI][buffJ] = stepBuffer[buffI][buffJ];
+      }
+    }
+    console.log(board);
+    return updateCells();
+  };
 
-  getLiveNeighbors = function(row, col) {};
+  getLiveNeighbors = function(row, col) {
+    var i, j, k, l, live, ref, ref1, ref2, ref3;
+    live = [];
+    for (i = k = ref = row - 1, ref1 = row + 1; ref <= ref1 ? k <= ref1 : k >= ref1; i = ref <= ref1 ? ++k : --k) {
+      for (j = l = ref2 = col - 1, ref3 = col + 1; ref2 <= ref3 ? l <= ref3 : l >= ref3; j = ref2 <= ref3 ? ++l : --l) {
+        if (isLive(i, j)) {
+          live.push([i, j]);
+        }
+      }
+    }
+    return live;
+  };
+
+  isLive = function(row, col) {
+    if (isInBounds(row, col)) {
+      return board[row][col] === true;
+    }
+    return false;
+  };
+
+  isInBounds = function(row, col) {
+    return row >= 0 && col >= 0 && row < board.length && col < board[row].length;
+  };
+
+  applyRules = function(cellLive, liveList) {
+    var numAlive;
+    numAlive = liveList.length;
+    if (numAlive < 2 && cellLive) {
+      return false;
+    }
+    if ((2 <= numAlive && numAlive <= 3) && cellLive) {
+      return true;
+    }
+    if (numAlive > 3 && cellLive) {
+      return false;
+    }
+    if (!cellLive && numAlive === 3) {
+      return true;
+    }
+  };
 
   canvas = document.getElementById('board');
 
@@ -82,8 +166,14 @@
 
   board = new Array(BOARD_HEIGHT + 2 * CALC_BUFFER);
 
-  for (i = j = 0, ref = board.length - 1; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+  for (i = k = 0, ref = board.length - 1; 0 <= ref ? k <= ref : k >= ref; i = 0 <= ref ? ++k : --k) {
     board[i] = new Array(BOARD_WIDTH + 2 * CALC_BUFFER);
+  }
+
+  stepBuffer = new Array(STEP_BUFFER_SIZE);
+
+  for (i = l = 0, ref1 = stepBuffer.length - 1; 0 <= ref1 ? l <= ref1 : l >= ref1; i = 0 <= ref1 ? ++l : --l) {
+    stepBuffer[i] = new Array(BOARD_WIDTH + 2 * CALC_BUFFER);
   }
 
   canvas.addEventListener('click', function(evt) {
@@ -95,8 +185,15 @@
   document.getElementById('step').addEventListener('click', update, false);
 
   document.getElementById('reset').addEventListener('click', function() {
+    var m, ref2, results;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    return drawGrid();
+    drawGrid();
+    board = new Array(BOARD_HEIGHT + 2 * CALC_BUFFER);
+    results = [];
+    for (i = m = 0, ref2 = board.length - 1; 0 <= ref2 ? m <= ref2 : m >= ref2; i = 0 <= ref2 ? ++m : --m) {
+      results.push(board[i] = new Array(BOARD_WIDTH + 2 * CALC_BUFFER));
+    }
+    return results;
   }, false);
 
 }).call(this);
